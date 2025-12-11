6ba383815cbd9d917db8e4e53a3d4e1ed25e20f7
Vikhyat Agrawal <vikhyat.agrawal@oracle.com>
2025-12-11 15:14:27 +0000

    Replace Old Event System with New Event system

--- wayflowcore/src/wayflowcore/executors/_executor.py
+++ wayflowcore/src/wayflowcore/executors/_executor.py
@@ -9,11 +9,8 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING, Optional, Sequence
 
-from wayflowcore.executors._events.event import Event, EventType
-
 if TYPE_CHECKING:
     from wayflowcore.conversation import Conversation
-    from wayflowcore.executors._executionstate import ConversationExecutionState
     from wayflowcore.executors.executionstatus import ExecutionStatus
     from wayflowcore.executors.interrupts.executioninterrupt import (
         ExecutionInterrupt,
@@ -30,32 +27,6 @@ class ExecutionInterruptedException(Exception):
 
 class ConversationExecutor(ABC):
     """Base Executor class. An executor is stateless, and exposes an execute method on the conversation."""
-
-    @staticmethod
-    def _register_event(
-        state: ConversationExecutionState,
-        conversation: Conversation,
-        event_type: EventType,
-    ) -> None:
-        new_event = Event(type=event_type)
-        state._register_event(new_event)
-        # we collect all the statuses to be sure to run all the callbacks, as they might update their internal status
-        interrupt_statuses = [
-            execution_interrupt.on_event(new_event, state, conversation)
-            for execution_interrupt in (state._get_execution_interrupts() or [])
-        ]
-        try:
-            # We look the first non-null interrupt status we find
-            execution_status = next(
-                interrupt_status
-                for interrupt_status in interrupt_statuses
-                if interrupt_status is not None
-            )
-            # If we find one, we raise an ExecutionInterruptedException containing the status
-            raise ExecutionInterruptedException(execution_status=execution_status)
-        except StopIteration:
-            # We did not find any non-null status, we can proceed
-            pass
 
     @staticmethod
     @abstractmethod
