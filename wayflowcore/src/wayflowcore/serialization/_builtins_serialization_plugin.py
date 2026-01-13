b5141bfdabff0faae6f82f21dcd53658e77ff39d
Youssef Boughizane <youssef.boughizane@oracle.com>
2026-01-13 08:37:20 +0100

    feat: added converter to SummarizationMessageTransform

--- wayflowcore/src/wayflowcore/serialization/_builtins_serialization_plugin.py
+++ wayflowcore/src/wayflowcore/serialization/_builtins_serialization_plugin.py
@@ -97,6 +97,9 @@ from pyagentspec.tools import RemoteTool as AgentSpecRemoteTool
 from pyagentspec.tools import ServerTool as AgentSpecServerTool
 from pyagentspec.tools import Tool as AgentSpecTool
 from pyagentspec.tools import ToolBox as AgentSpecToolBox
+from pyagentspec.transforms import (
+    MessageSummarizationTransform as AgentSpecMessageSummarizationTransform,
+)
 from pyagentspec.transforms import MessageTransform as AgentSpecMessageTransform
 
 from wayflowcore._metadata import METADATA_KEY
@@ -439,7 +442,10 @@ from wayflowcore.transforms.canonicalizationtransform import (
     CanonicalizationMessageTransform as RuntimeCanonicalizationMessageTransform,
 )
 from wayflowcore.transforms.transforms import (
-    SplitPromptOnMarkerMessageTransform as RuntimeSplitPromptOnMarkerMessageTransform,
+    SplitPromptOnMarkerMessageTransform as RuntimeSplitPromptOnMarkerMessageTransform
+)
+from wayflowcore.transforms.summarization import (
+    MessageSummarizationTransform as RuntimeMessageSummarizationTransform,
 )
 from wayflowcore.variable import Variable as RuntimeVariable
 
@@ -1480,8 +1486,8 @@ class WayflowBuiltinsSerializationPlugin(WayflowSerializationPlugin):
         elif isinstance(runtime_messagetransform, RuntimeCanonicalizationMessageTransform):
             return AgentSpecPluginCanonicalizationMessageTransform(
                 name="canonicalization_messagetransform",
-                metadata=_create_agentspec_metadata_from_runtime_component(
-                    runtime_messagetransform
+                    metadata=_create_agentspec_metadata_from_runtime_component(
+                        runtime_messagetransform
                 ),
             )
         elif isinstance(runtime_messagetransform, RuntimeSplitPromptOnMarkerMessageTransform):
@@ -1492,6 +1498,23 @@ class WayflowBuiltinsSerializationPlugin(WayflowSerializationPlugin):
                 ),
                 marker=runtime_messagetransform.marker,
             )
+        elif isinstance(runtime_messagetransform, RuntimeMessageSummarizationTransform):
+            return AgentSpecMessageSummarizationTransform(
+                name=runtime_messagetransform.name or "message-summarizer",
+                llm=self._llm_convert_to_agentspec(
+                    conversion_context, runtime_messagetransform._summarizer.llm, referenced_objects
+                ),
+                max_message_size=runtime_messagetransform.max_message_size,
+                summarization_instructions=runtime_messagetransform._summarizer.summarization_instructions,
+                summarized_message_template=runtime_messagetransform._summarizer.summarized_content_template,
+                max_cache_size=runtime_messagetransform.cache.max_cache_size,
+                max_cache_lifetime=runtime_messagetransform.cache.max_cache_lifetime,
+                cache_collection_name=runtime_messagetransform.cache.collection_name,
+                metadata=_create_agentspec_metadata_from_runtime_component(
+                    runtime_messagetransform
+                ),
+            )
+
         raise ValueError(
             f"Unsupported type of MessageTransform in Agent Spec: {type(runtime_messagetransform)}"
         )
