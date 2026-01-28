8c517ad2aeaf701ae3fd64634928d65eede54eff
Sathvik Bhagavan <sathvik.bhagavan@oracle.com>
2026-01-28 11:54:16 +0100

    chore: fix rebase mistakes

--- wayflowcore/src/wayflowcore/steps/agentexecutionstep.py
+++ wayflowcore/src/wayflowcore/steps/agentexecutionstep.py
@@ -262,7 +262,7 @@ class AgentExecutionStep(Step):
         if isinstance(self.agent, Agent):
             return self.agent.might_yield or self.caller_input_mode == CallerInputMode.ALWAYS
         elif isinstance(self.agent, (Swarm, ManagerWorkers)):
-            # Swarm's caller_input_mode enforces whether the agent is conversational or not
+            # Swarm/ManagerWorker's caller_input_mode enforces whether the agent is conversational or not
             return (
                 self.agent.caller_input_mode == CallerInputMode.ALWAYS
                 or self.caller_input_mode == CallerInputMode.ALWAYS
