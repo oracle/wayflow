951d7f2fa0918920694af825e4c5ccbbdee83765
Sathvik Bhagavan <sathvik.bhagavan@oracle.com>
2026-01-30 08:43:08 +0100

    fix: convert from agentspec to wayflow

--- wayflowcore/src/wayflowcore/managerworkers.py
+++ wayflowcore/src/wayflowcore/managerworkers.py
@@ -125,8 +125,8 @@ class ManagerWorkers(ConversationalComponent, SerializableDataclassMixin, Serial
 
         self.workers = workers
 
-        self._agent_by_name: Dict[str, Union["Agent", "ManagerWorkers"]] = _validate_agent_unicity(
-            self.workers, self.manager_agent
+        self._agent_by_name: Dict[str, Union["Agent", "ManagerWorkers", "A2AAgent"]] = (
+            _validate_agent_unicity(self.workers, self.manager_agent)
         )
 
         # Create send message tools for the group manager
