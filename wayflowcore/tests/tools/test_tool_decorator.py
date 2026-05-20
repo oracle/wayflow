556156fd6c3e6284a54768a4f65347ff3c0cf429
soufianeamini <soufiane.amini@oracle.com>
2026-05-20 13:30:19 +0100

    Improve tests and add descriptive comments

--- wayflowcore/tests/tools/test_tool_decorator.py
+++ wayflowcore/tests/tools/test_tool_decorator.py
@@ -5,18 +5,30 @@ from wayflowcore.tools.toolhelpers import DescriptionMode, tool
 
 
 def test_tool_decorator_result_type_is_correct() -> None:
+    # Wrapper with different name
     tool_one = tool("tool_one")
     assert_type(tool_one, Callable[[Callable[..., Any]], ServerTool])
     assert isinstance(tool_one, Callable)
 
     def actual_func() -> None:
-        """Another func"""
+        """Actual func"""
 
     func_tool = tool_one(actual_func)
     assert_type(func_tool, ServerTool)
     assert isinstance(func_tool, ServerTool)
     assert func_tool.name == "tool_one"
 
+    # Decorator with different tool name
+
+    @tool("real_function_name")
+    def another_func() -> None:
+        """Another func"""
+
+    assert_type(another_func, ServerTool)
+    assert isinstance(another_func, ServerTool)
+    assert another_func.name == "real_function_name"
+
+    # Wrapper with name and function passed as arguments
     def func_two() -> None:
         """Just a func"""
 
@@ -24,16 +36,18 @@ def test_tool_decorator_result_type_is_correct() -> None:
     assert_type(tool_two, ServerTool)
     assert isinstance(tool_two, ServerTool)
 
+    # Decorator with description mode
     @tool("tool_three", description_mode=DescriptionMode.ONLY_DOCSTRING)
     def tool_three() -> None:
         """tool_three function"""
 
+    assert_type(tool_three, ServerTool)
+    assert isinstance(tool_three, ServerTool)
+
+    # Decorator with no arguments passed
     @tool
     def tool_four() -> None:
         """tool_four function"""
 
-    assert_type(tool_three, ServerTool)
     assert_type(tool_four, ServerTool)
-
-    assert isinstance(tool_three, ServerTool)
     assert isinstance(tool_four, ServerTool)
